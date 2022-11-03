e7c56dfef002ff74666d0e055306e7c56abed7d6
Haley <57505566+HaleyRovner@users.noreply.github.com>
2022-11-03 09:57:29 -0700

    saved old code, added assets folder, got background image working

--- lib/main.dart
+++ lib/main.dart
@@ -10,46 +10,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-            title: const Text("Portland Yard"),
-            backgroundColor: const Color.fromRGBO(81, 52, 45, 1),
-            iconTheme:
-                const IconThemeData(color: Color.fromRGBO(164, 119, 91, 1))),
-        drawer: Drawer(
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(81, 52, 45, 1),
-                ),
-                child: Text('Settings'),
-              ),
-              ListTile(
-                title: const Text('Game Settings'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: const Text('Something'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        ),
-        body: const MyStatefulWidget(),
+        body: MyStatefulWidget(),
       ),
     );
   }
@@ -65,31 +28,38 @@ class MyStatefulWidget extends StatefulWidget {
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle style =
-        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ElevatedButton(
-            style: style,
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const GamePage()),
-              );
-            },
-            child: const Text('Start Game'),
+    //final ButtonStyle style =
+    //ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/home_background.jpeg"),
+              fit: BoxFit.cover,
+            ),
           ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            style: style,
-            onPressed: () {},
-            child: const Text('Join Game'),
+          child: null
+          // child: Column(
+          //   mainAxisSize: MainAxisSize.min,
+          //   children: <Widget>[
+          //     ElevatedButton(
+          //       style: style,
+          //       onPressed: () {
+          //         Navigator.push(
+          //           context,
+          //           MaterialPageRoute(builder: (context) => const GamePage()),
+          //         );
+          //       },
+          //       child: const Text('Start Game'),
+          //     ),
+          //     const SizedBox(height: 30),
+          //     ElevatedButton(
+          //       style: style,
+          //       onPressed: () {},
+          //       child: const Text('Join Game'),
+          //     ),
+          //],
           ),
-        ],
-      ),
     );
   }
 }
