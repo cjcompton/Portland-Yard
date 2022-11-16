3d03071418f497983d29fe8330ce8b635d68ec28
Haley <57505566+HaleyRovner@users.noreply.github.com>
2022-11-16 08:26:39 -0800

    added Create Game features

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:dropdown_textfield/dropdown_textfield.dart';
 //import 'package:numberpicker/numberpicker.dart';
 
 void main() {
@@ -129,32 +130,66 @@ class CreateGamePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        textStyle: const TextStyle(
+            fontFamily: 'Oswald', fontSize: 24.0, color: Colors.white));
+
+    //var _yesNoOptions = ["yes", "no"];
+    //var _currentSelectedValue = 'Randomly Assigned Mr. X?';
+
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(196, 142, 125, 100),
-          title: const Text("Create Game"),
-        ),
         body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            padding: const EdgeInsets.symmetric(vertical: 75, horizontal: 30),
             decoration:
                 const BoxDecoration(color: Color.fromRGBO(118, 156, 145, 100)),
-            padding: const EdgeInsets.all(25),
-            child: const SizedBox(
-              width: 50,
-              height: 50,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: '_',
-                ),
-
-                /*ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text("go back"),
-        ),*/
-              ),
-            )));
+            child: Column(children: <Widget>[
+              const Text('Create Game',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: "Oswald-SemiBold",
+                      fontSize: 35,
+                      color: Colors.white)),
+              ElevatedButton(
+                  style: style,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Go Back')),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 5.0, horizontal: 50.0)),
+              Container(
+                color: Colors.black.withOpacity(0.5),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+                child: Column(children: const <Widget>[
+                  /*SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: */
+                  TextField(
+                      decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Number of Players',
+                  )) /*)*/,
+                  SizedBox(height: 15),
+                  Divider(height: 0, thickness: 1, color: Colors.white),
+                  SizedBox(height: 15),
+                  TextField(
+                      decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Number of Bots',
+                  )),
+                  SizedBox(height: 15),
+                  Divider(height: 0, thickness: 1, color: Colors.white),
+                  SizedBox(height: 15),
+                ]),
+              )
+            ])));
   }
 }
 
