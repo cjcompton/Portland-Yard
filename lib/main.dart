6446fe8627a138db118543925abb03ca46ea5f54
Haley <57505566+HaleyRovner@users.noreply.github.com>
2022-11-29 23:00:05 -0800

    fixed spacing

--- lib/main.dart
+++ lib/main.dart
@@ -283,10 +283,10 @@ class JoinGamePage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 15),
-                  ElevatedButton(
+                  /*ElevatedButton(
                     onPressed: null,
                     child: Text('Disabled'),
-                  ),
+                  ),*/
                 ]),
               )
             ])));
@@ -314,22 +314,52 @@ class GamePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        textStyle: const TextStyle(
+            fontFamily: 'Oswald', fontSize: 24.0, color: Colors.white));
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        padding: const EdgeInsets.symmetric(vertical: 75, horizontal: 30),
-        decoration:
-            const BoxDecoration(color: Color.fromRGBO(118, 156, 145, 100)),
-        child: Column(children: const <Widget>[
-          Text('Game Page',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontFamily: "Oswald-SemiBold",
-                  fontSize: 35,
-                  color: Colors.white)),
-        ]),
-      ),
-    );
+        body: Container(
+            color: const Color.fromRGBO(88, 44, 38, 100).withOpacity(0.7),
+            alignment: Alignment.center,
+            //padding: EdgeInsets.all((MediaQuery.of(context).size.width) / 4),
+            padding: const EdgeInsets.symmetric(vertical: 65, horizontal: 20),
+            child: Column(children: <Widget>[
+              const Text('Game Page',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: "Oswald-SemiBold",
+                      fontSize: 35,
+                      color: Colors.white)),
+              ElevatedButton(
+                  style: style,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Go Back')),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 5.0, horizontal: 50.0)),
+              Container(
+                color: Colors.black.withOpacity(0.5),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+                child: Column(children: <Widget>[
+                  /*SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: */
+                  Container(
+                      height: 350,
+                      width: 375,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage('assets/images/map.jpeg'),
+                        fit: BoxFit.fill,
+                      )))
+                ]),
+              )
+            ])));
   }
 }
