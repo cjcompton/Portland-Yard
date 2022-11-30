d446554880caa5eddb8b4f1941fe3c513c91148a
Haley <57505566+HaleyRovner@users.noreply.github.com>
2022-11-30 06:46:31 -0800

    started making framework for game page

---
+++ lib/portland-game.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class PortlandGame extends Game {
+  late Size screenSize;
+  void render(Canvas canvas) {}
+  void update(double t) {}
+  void resize(Size size) {}
+}
