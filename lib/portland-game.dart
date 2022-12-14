ee824bb8a9243f3e3fd12bb21e54022713726bd4
Haley <57505566+HaleyRovner@users.noreply.github.com>
2022-12-13 22:52:36 -0800

    removed game file since it was breaking

--- lib/portland-game.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-import 'package:flame/game.dart';
-
-class PortlandGame extends Game {
-  late Size screenSize;
-  void render(Canvas canvas) {}
-  void update(double t) {}
-  void resize(Size size) {}
-}
